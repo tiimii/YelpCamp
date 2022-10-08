30e9ad0ce8786a0a5781f6e6fe89eacb7095664b
Timothy Bastow <timothy.bastow@outlook.com>
2022-10-08 18:36:20 +0200

    redefined style for all pages. Added Unspash API to get random images.
    Used the bootstrap cards for listing the all the campsites as well as for showing individual sites. Reformatted the new and edit campground pages. Added some fields for camprounds such as price, image and description. Installed the Unsplash package in order to get images from the Unsplash API. Added the node-fetch@v2 package in order to get the Unspash API working.

--- seeds/index.js
+++ seeds/index.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose')
 const Campground = require('../models/campground')
 const cities = require('./cities')
 const {places, descriptors} = require('./seedHelpers')
+const nodeFetch = require('node-fetch')
+const { createApi } = require('unsplash-js')
 
 // Connecting to database
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
@@ -12,20 +14,47 @@ db.once('open', () => {
     console.log('Database connected.')
 })
 
+// Setting up the Unsplash packages for images
+const unsplash = createApi({
+    accessKey: 'qkTJ7emoVD75xjnrdHiC4Fj57pNwPpB5jJHrkcBgRh8',
+    fetch: nodeFetch
+})
+
 const sample = (array) => array[Math.floor(Math.random() * array.length)]
 
 const seedDB = async() => {
+    // Clear the existing database
     await Campground.deleteMany({})
-    for (let i = 0; i <50; i++) {
-        const random1000 = Math.floor(Math.random() * 1000)
-        const camp = new Campground({
-            location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
-        })
-        await camp.save()
+
+    // defining how many campgrounds we wish to generate
+    const campgroundNumber = 10
+
+    // Get random images from the Unsplash API
+    const imageArray = await unsplash.photos.getRandom({
+        collectionIds: ['429524'],
+        count: campgroundNumber
+    })
+
+    // Loop to create random campgrounds
+    for (let i = 0; i < campgroundNumber; i++) {
+        try {
+            const random1000 = Math.floor(Math.random() * 1000)
+            const price = Math.floor(Math.random() * 20) + 10
+            const camp = new Campground({
+                location: `${cities[random1000].city}, ${cities[random1000].state}`,
+                title: `${sample(descriptors)} ${sample(places)}`,
+                image: imageArray.response[i].urls.regular,
+                description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis molestiae enim inventore consequatur adipisci. Facere doloremque aliquid sed dicta laborum nemo ratione tempore nisi perspiciatis? Incidunt harum expedita et? Aliquam.',
+                price: price
+            })
+            await camp.save()
+        } catch (error) {
+            console.log(error)
+        }
     }
 }
 
 seedDB().then(() => {
     mongoose.connection.close()
+    console.log('database updated')
 })
